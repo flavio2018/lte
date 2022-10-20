bcd281781f5fee09a159795cab4ef27b2347f9af
Flavio <flavio.petruzzellis@gmail.com>
2022-10-20 16:41:47 +0200

    Add valid step train lstm

--- src/train_lstm.py
+++ src/train_lstm.py
@@ -57,6 +57,17 @@ def train_lstm():
 		log_weights_gradient(model, step)
 		log_params_norm(model, step)
 
+		if i_step % 100 == 0:
+			n_valid = i_step / 100
+			for v_step in range(10):
+				padded_samples_batch, padded_targets_batch, samples_len, targets_len = generate_batch(length=LEN, nesting=NES, batch_size=BS, split='valid')
+				padded_samples_batch, padded_targets_batch = padded_samples_batch.to(DEVICE), padded_targets_batch.to(DEVICE)
+				loss_valid_step = valid_step(model, padded_samples_batch, padded_targets_batch, samples_len, targets_len, loss, DEVICE)
+				wandb.log({
+					"val_loss": loss_valid_step,
+					"val_update": n_valid*10 + v_step,
+				})
+
 
 def step(model, sample, target, samples_len, targets_len, loss, opt, device):
 	opt.zero_grad()
@@ -98,6 +109,46 @@ def step(model, sample, target, samples_len, targets_len, loss, opt, device):
 	avg_loss.backward()
 	opt.step()
 
+	model.detach_states()
+	return avg_loss.item()
+
+
+def valid_step(model, sample, target, samples_len, targets_len, loss, device):
+	model.eval()
+	outputs = []
+	first_output = {}
+	h_dict, c_dict = {}, {}
+	samples_len = samples_len.copy()
+	targets_len = targets_len.copy()
+	hid_size = model.h_t_1.size(1)
+
+	for char_pos in range(sample.size(1)):
+		hidden_mask = get_hidden_mask(samples_len, hid_size, device)
+		output = model(sample[:, char_pos, :].squeeze(), hidden_mask)
+		samples_len = reduce_lens(samples_len)
+		h_dict, c_dict = save_states(model, h_dict, c_dict, samples_len)
+		first_output = populate_first_output(output, samples_len, first_output)
+	outputs.append(build_first_output(first_output))
+	
+	model.set_states(h_dict, c_dict)
+	
+	targets_len_copy = targets_len.copy()
+	for char_pos in range(target.size(1) - 1):
+		hidden_mask = get_hidden_mask(targets_len_copy, hid_size, device)
+		output = model(target[:, char_pos, :].squeeze(), hidden_mask)
+		targets_len_copy = reduce_lens(targets_len_copy)
+		outputs.append(output)
+	
+	count_nonzero = 0
+	cumulative_loss = 0
+	for char_pos, output in enumerate(outputs):
+		loss_mask = get_mask(targets_len, device)
+		targets_len = reduce_lens(targets_len)
+		char_loss = loss(output, torch.argmax(target[:, char_pos, :].squeeze(), dim=1)) * loss_mask
+		count_nonzero += (char_loss != 0).sum()
+		cumulative_loss += torch.sum(char_loss)
+	avg_loss = cumulative_loss / count_nonzero
+	
 	model.detach_states()
 	return avg_loss.item()
 

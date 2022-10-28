8409a795900f88b2754c8809547604b3a63f71e1
Flavio <flavio.petruzzellis@gmail.com>
2022-10-28 16:31:11 +0200

    Add encdec testing funcs

--- src/model/test.py
+++ src/model/test.py
@@ -73,6 +73,37 @@ def dntm_fwd_padded_batch(model, sample, target, samples_len, targets_len, devic
 	return outputs
 
 
+@torch.no_grad()
+def encdec_fwd_padded_batch(encoder, decoder, sample, target, samples_len, targets_len, device):
+    outputs = []
+    h_dict, c_dict = {1: {}, 2: {}}, {1: {}, 2: {}}
+    samples_len = samples_len.copy()
+    targets_len = targets_len.copy()
+    hid_size = encoder.h_t_1.size(1)
+    
+    for char_pos in range(sample.size(1)):
+        hidden_mask = get_hidden_mask(samples_len, hid_size, device)
+        output = encoder(sample[:, char_pos, :].squeeze(), hidden_mask)
+        samples_len = reduce_lens(samples_len)
+        h_dict, c_dict = save_states(encoder, h_dict, c_dict, samples_len)
+        
+    decoder.set_states(h_dict, c_dict)
+    output = decoder(torch.ones(sample[:, char_pos, :].squeeze().size(), device=device),
+                     torch.ones(hidden_mask.size(), device=device))
+    outputs.append(output)
+    targets_len_copy = targets_len.copy()
+    targets_len_copy = reduce_lens(targets_len_copy)
+
+    for char_pos in range(target.size(1) - 1):
+        hidden_mask = get_hidden_mask(targets_len_copy, hid_size, device)
+        output = decoder(target[:, char_pos, :].squeeze(), hidden_mask)
+        targets_len_copy = reduce_lens(targets_len_copy)
+        outputs.append(output)
+    encoder.detach_states()
+    decoder.detach_states()
+    return outputs
+
+
 def eval_padded(outputs, target, sample):
 	BS = target.size(0)
 
@@ -95,3 +126,7 @@ def eval_lstm_padded(model, padded_samples_batch, padded_targets_batch, samples_
 def eval_dntm_padded(model, padded_samples_batch, padded_targets_batch, samples_len, targets_len, device):
 	outputs = dntm_fwd_padded_batch(model, padded_samples_batch, padded_targets_batch, samples_len, targets_len, device)
 	eval_padded(outputs, padded_targets_batch, padded_samples_batch)
+
+def eval_encdec_padded(encoder, decoder, padded_samples_batch, padded_targets_batch, samples_len, targets_len, device):
+    outputs = encdec_fwd_padded_batch(encoder, decoder, padded_samples_batch, padded_targets_batch, samples_len, targets_len, device)
+    eval_padded(outputs, padded_targets_batch, padded_samples_batch)

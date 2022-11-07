66663f8a2f6c07bbb91aa08673ac56ac789ba9e1
Flavio <flavio.petruzzellis@gmail.com>
2022-11-07 12:08:02 +0100

    Update scripts

--- src/scripts/encdec_2048.sh
+++ src/scripts/encdec_2048.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 python train_encdec.py codename=encdec_if max_len=1 max_nes=1 ops=i
-python train_encdec.py codename=encdec_eos_sum_sub max_len=1 max_nes=2 hid_size=2048
-python train_encdec.py codename=encdec_eos_sum_sub max_len=1 max_nes=3 hid_size=2048
+python train_encdec.py codename=encdec_eos_sum_sub max_len=1 max_nes=2 ops=as hid_size=2048
+python train_encdec.py codename=encdec_eos_sum_sub max_len=1 max_nes=3 ops=as hid_size=2048
 python train_encdec.py codename=encdec_2048 max_len=1 max_nes=1 ops=ai hid_size=2048
 python train_encdec.py codename=encdec_2048 max_len=2 max_nes=1 ops=a hid_size=2048

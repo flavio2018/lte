e8be61b31778c8f2d9decb6e9d1a53968a2b9833
Flavio <flavio.petruzzellis@gmail.com>
2022-11-24 18:57:17 +0100

    Edit train_encdec
    - remove old imports
    - remove functions for combine CL
    - add tracking num_unequal

--- src/train_encdec.py
+++ src/train_encdec.py
@@ -2,9 +2,9 @@
 
 from model.lstm import LSTM, DeepLSTM
 from model.mlp import MLP
-from model.test import eval_encdec_padded, compute_loss, encdec_step
-from data.generator import get_vocab_size, get_target_vocab_size, generate_batch, SubsetDataset
-from utils.rnn_utils import get_mask, get_hidden_mask, reduce_lens, save_states, populate_first_output, build_first_output, batch_acc
+from model.test import eval_encdec_padded, compute_loss, encdec_step, get_num_unequal
+from data.generator import get_vocab_size, get_target_vocab_size, generate_batch
+from utils.rnn_utils import batch_acc
 from utils.wandb_utils import log_weights_gradient, log_params_norm
 import collections
 import numpy as np
@@ -48,7 +48,7 @@ def train_encdec(cfg):
 
 	loss = torch.nn.CrossEntropyLoss(reduction='none')
 	opt = torch.optim.Adam(enc_dec_parameters, lr=cfg.lr)
-	lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=opt, gamma=0.9)
+	lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=opt, gamma=1)
 	FREQ_LR_DECAY = cfg.max_iter // 100
 
 	wandb.init(
@@ -89,14 +89,8 @@ def train_encdec(cfg):
 
 		padded_samples_batch, padded_targets_batch, samples_len, targets_len = generate_batch(LEN, NES, cfg.bs, split='valid', ops=cfg.ops, mod=cfg.mod)
 		padded_samples_batch, padded_targets_batch = padded_samples_batch.to(cfg.device), padded_targets_batch.to(cfg.device)
-		loss_valid_step, acc_valid_step = valid_step(encoder, decoder, final_mlp, padded_samples_batch, padded_targets_batch, samples_len, targets_len, loss, cfg.device)
-		if i_step % FREQ_LOSS_RECORDING == 0:
-			losses.append(loss_valid_step)
-			LEN, NES, losses = get_len_nes(LEN, NES, losses, cfg)
-
-		if acc_valid_step >= 0.9 and not cfg.cl_mix:
-			LEN += 1
-
+		loss_valid_step, acc_valid_step, _ = valid_step(encoder, decoder, final_mlp, padded_samples_batch, padded_targets_batch, samples_len, targets_len, loss, cfg.device)
+		
 		wandb.log({
 			"val_loss": loss_valid_step,
 			"val_acc": acc_valid_step,
@@ -109,10 +103,11 @@ def train_encdec(cfg):
 		if i_step % FREQ_EVAL == 0:
 			padded_samples_batch, padded_targets_batch, samples_len, targets_len = generate_batch(cfg.max_len, cfg.max_nes, cfg.bs, split='test', ops=cfg.ops, mod=cfg.mod)
 			padded_samples_batch, padded_targets_batch = padded_samples_batch.to(cfg.device), padded_targets_batch.to(cfg.device)
-			_, acc_test = valid_step(encoder, decoder, final_mlp, padded_samples_batch, padded_targets_batch, samples_len, targets_len, loss, cfg.device)
+			_, acc_test, num_unequal = valid_step(encoder, decoder, final_mlp, padded_samples_batch, padded_targets_batch, samples_len, targets_len, loss, cfg.device)
 			wandb.log({
 				"acc_test": acc_test,
 				"test_update": i_step // FREQ_EVAL,
+				"num_unequal": num_unequal,
 			})
 		
 
@@ -142,33 +137,11 @@ def valid_step(encoder, decoder, final_mlp, sample, target, samples_len, targets
 	outputs = encdec_step(encoder, decoder, final_mlp, sample, target, samples_len, targets_len, device)
 	avg_loss = compute_loss(loss, outputs, target[:, 1:, :])
 	acc = batch_acc(outputs, target[:, 1:, :], get_target_vocab_size())
+	num_unequal = get_num_unequal(outputs, target[:, 1:, :])
 	
 	encoder.detach_states()
 	decoder.detach_states()
-	return avg_loss.item(), acc.item()
-
-
-def increase_len_nes(losses):
-	if len(losses) < LOSS_WIN_SIZE:
-		return False
-
-	# Average change in loss normalized by average loss.
-	loss_diffs = [pair[0] - pair[1]
-				  for pair in zip(list(losses)[1:],
-								  list(losses)[:-1])]
-	avg_loss_norm = np.mean(loss_diffs) / np.mean(losses)
-	if avg_loss_norm < 0.05:
-		return True
-	return False
-
-
-def get_len_nes(cur_len, cur_nes, losses, cfg):
-	if increase_len_nes(losses):
-		losses = collections.deque([], maxlen=LOSS_WIN_SIZE)
-		if cur_len < cfg.max_len and cur_nes < cfg.max_nes:
-			print(f"Increased len to {cur_len+1}, nes to {cur_nes+1}")
-			return cur_len+1, cur_nes+1, losses
-	return cur_len, cur_nes, losses
+	return avg_loss.item(), acc.item(), num_unequal
 
 
 if __name__ == '__main__':

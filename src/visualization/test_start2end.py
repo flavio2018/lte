31025b261fc459b86b79b48c2210d84318959c30
Flavio <flavio.petruzzellis@gmail.com>
2023-03-02 16:12:25 +0100

    Improve printout

--- src/visualization/test_start2end.py
+++ src/visualization/test_start2end.py
@@ -91,11 +91,11 @@ def model_output_to_next_input(cur_input, output, output_tensor, running):
 	logging.info(f"{running.sum()} outputs are running.")
 	chararray_outputs = cut_at_first_dot(chararray_outputs, running)
 	outputs_are_well_formed = contain_one_space(chararray_outputs)
-	logging.info(f"{(~outputs_are_well_formed).sum()} outputs are not well formed.")
-	logging.info([f"{i} → {o}" for i, o in zip(chararray_inputs[~outputs_are_well_formed][:20], 
-		chararray_outputs[~outputs_are_well_formed][:20])])
+	logging.info(f"{(~outputs_are_well_formed & running).sum()} outputs are not well formed.")
+	logging.info([f"{i} → {o}" for i, o in zip(chararray_inputs[~outputs_are_well_formed & running][:20], 
+		chararray_outputs[~outputs_are_well_formed & running][:20])])
 	logging.info("Top 2 logits for first 10 ill-formed model outputs")
-	top2_logits, _ = output_tensor[torch.tensor(~outputs_are_well_formed,
+	top2_logits, _ = output_tensor[torch.tensor(~outputs_are_well_formed & running,
 											 device=output_tensor.device)][:10].topk(k=2, dim=-1)
 	logging.info(top2_logits)
 	running &= outputs_are_well_formed
@@ -103,9 +103,13 @@ def model_output_to_next_input(cur_input, output, output_tensor, running):
 	
 	# check substring in input
 	inputs_do_contain_substrings = inputs_contain_substrings(chararray_inputs, chararray_outputs, running)
-	logging.info(f"{(~inputs_do_contain_substrings).sum()} outputs have wrong substrings.")
-	logging.info([f"{i} → {o}" for i, o in zip(chararray_inputs[~inputs_do_contain_substrings][:20], 
-		chararray_outputs[~inputs_do_contain_substrings][:20])])
+	logging.info(f"{(~inputs_do_contain_substrings & running).sum()} outputs have wrong substrings.")
+	logging.info([f"{i} → {o}" for i, o in zip(chararray_inputs[~inputs_do_contain_substrings & running][:20], 
+		chararray_outputs[~inputs_do_contain_substrings & running][:20])])
+	logging.info("Top 2 logits for first 10 no-substring model outputs")
+	top2_logits, _ = output_tensor[torch.tensor(~inputs_do_contain_substrings & running,
+											 device=output_tensor.device)][:10].topk(k=2, dim=-1)
+	logging.info(top2_logits)
 	running &= inputs_do_contain_substrings
 	logging.info(f"{running.sum()} outputs are running.")
 

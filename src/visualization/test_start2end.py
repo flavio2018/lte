3f34e736750ad4479925f3b903fb80d569759228
Flavio <flavio.petruzzellis@gmail.com>
2023-03-07 14:57:27 +0100

    Change errors logging

--- src/visualization/test_start2end.py
+++ src/visualization/test_start2end.py
@@ -148,11 +148,13 @@ def model_output_to_next_input(cur_input, output, output_tensor, running, use_tr
 		chararray_outputs = replace_double_spaces(chararray_outputs)
 		outputs_are_well_formed = contain_one_space(chararray_outputs)
 		logging.info(f"{(~outputs_are_well_formed & running).sum()} outputs are not well formed after double space correction.")
-	logging.info('\n'.join([f"{i} → {o}" for i, o in zip(chararray_inputs[~outputs_are_well_formed & running][:20], 
-		chararray_outputs[~outputs_are_well_formed & running][:20])]))
+	well_formed_running_inputs = chararray_inputs[~outputs_are_well_formed & running]
+	log_idx = np.random.choice(well_formed_running_inputs.shape[0], size=20, replace=False)
+	logging.info('\n'.join([f"{i} → {o}" for i, o in zip(well_formed_running_inputs[log_idx], 
+		chararray_outputs[~outputs_are_well_formed & running][log_idx])]))
 	logging.info("Top 2 logits for first 10 ill-formed model outputs")
 	top2_logits, _ = output_tensor[torch.tensor(~outputs_are_well_formed & running,
-											 device=output_tensor.device)][:10].topk(k=2, dim=-1)
+											 device=output_tensor.device)][torch.tensor(log_idx[:10])].topk(k=2, dim=-1)
 	logging.info(top2_logits)
 	running &= outputs_are_well_formed
 	logging.info(f"{running.sum()} outputs are running.")
@@ -160,11 +162,13 @@ def model_output_to_next_input(cur_input, output, output_tensor, running, use_tr
 	# check substring in input
 	inputs_do_contain_substrings = inputs_contain_substrings(chararray_inputs, chararray_outputs, running)
 	logging.info(f"{(~inputs_do_contain_substrings & running).sum()} outputs have wrong substrings.")
-	logging.info('\n'.join([f"{i} → {o}" for i, o in zip(chararray_inputs[~inputs_do_contain_substrings & running][:20], 
-		chararray_outputs[~inputs_do_contain_substrings & running][:20])]))
+	inputs_with_substring_running = chararray_inputs[~inputs_do_contain_substrings & running]
+	log_idx = np.random.choice(inputs_with_substring_running.shape[0], size=20, replace=False)
+	logging.info('\n'.join([f"{i} → {o}" for i, o in zip(inputs_with_substring_running[log_idx], 
+		chararray_outputs[~inputs_do_contain_substrings & running][log_idx])]))
 	logging.info("Top 2 logits for first 10 no-substring model outputs")
 	top2_logits, _ = output_tensor[torch.tensor(~inputs_do_contain_substrings & running,
-											 device=output_tensor.device)][:10].topk(k=2, dim=-1)
+											 device=output_tensor.device)][torch.tensor(log_idx[:10])].topk(k=2, dim=-1)
 	logging.info(top2_logits)
 	if use_tricks:
 		inputs_do_soft_contain_substrings = inputs_soft_contain_substrings(chararray_inputs, chararray_outputs, running)

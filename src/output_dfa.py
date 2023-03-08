7e0b15876bc94739694c562697b26dd8e728dedb
Flavio <flavio.petruzzellis@gmail.com>
2023-03-08 13:15:55 +0100

    Create output DFA

---
+++ src/output_dfa.py
@@ -0,0 +1,196 @@
+{
+	('-'): {	# negative first number
+		('123456789'): {	
+			('0123456789'): {	# two digits
+				(' '): {
+					('('): {
+						('-'): {	# negative first expression number
+							('123456789'): {
+								('0123456789'): {	# two digits
+									('+-*'): {
+										('123456789'): {	# second expr op
+											('0123456789'): {	# two digits
+												(')'): '.'
+											},
+											(')'): '.'	# one digit	
+										}
+									}
+								},
+								('+-*'): {	# one digit
+									('123456789'): {	# second expr op
+										('0123456789'): {	# two digits
+											(')'): '.'
+										},
+										(')'): '.'	# one digit	
+									}
+								}
+							}
+						},
+						('123456789'): {	# positive first expression number
+							('0123456789'): {	# two digits
+								('+-*'): {
+									('123456789'): {	# second expr op
+										('0123456789'): {	# two digits
+											(')'): '.'
+										},
+										(')'): '.'	# one digit	
+									}
+								}
+							},
+							('+-*'): {	# one digit
+								('123456789'): {	# second expr op
+									('0123456789'): {	# two digits
+										(')'): '.'
+									},
+									(')'): '.'	# one digit	
+								}
+							}
+						}
+					}
+				}
+			},
+			(' '): {	# one digit
+				('('): {
+					('-'): {	# negative first expr op
+						('123456789'): {
+							('0123456789'): {	# two digits
+								('+-*'): {
+									('123456789'): {	# second expr op
+										('0123456789'): {	# two digits
+											(')'): '.'
+										},
+										(')'): '.'	# one digit	
+									}
+								}
+							},
+							('+-*'): {	# one digit
+								('123456789'): {	# second expr op
+									('0123456789'): {	# two digits
+										(')'): '.'
+									},
+									(')'): '.'	# one digit	
+								}
+							}
+						}
+					},
+					('123456789'): {	# positive first expr op
+						('0123456789'): {	# two digits
+							('+-*'): {
+								('123456789'): {	# second expr op
+									('0123456789'): {	 # two digits
+										(')'): '.'
+									},
+									(')'): '.'	# one digit	
+								}
+							}
+						},
+						('+-*'): {	# one digit
+							('123456789'): {	# second expr op
+								('0123456789'): {	# two digits
+									(')'): '.'
+								},
+								(')'): '.'	# one digit		
+							}
+						}	
+					}
+				}
+			}
+		},
+	},
+	('123456789'): {	 # positive first number
+		('0123456789'): {	# two digits
+			(' '): {
+				('('): {
+					('-'): {	# negative first expr number
+						('123456789'): {
+							('0123456789'): {	# two digits
+								('+-*'): {
+									('123456789'): {
+										('0123456789'): {	# two digits
+											(')'): '.'
+										},
+										(')'): '.'	# one digit
+									}
+								}
+							},
+							('+-*'): {	# one digit
+								('123456789'): {
+									('0123456789'): {	# two digits
+										(')'): '.'
+									},
+									(')'): '.'	# one digit
+								}
+							}	
+						}
+					},
+					('123456789'): {	# positive first expr number
+						('0123456789'): {	# two digits
+							('+-*'): {
+								('123456789'): {
+									('0123456789'): {	# two digits
+										(')'): '.'
+									},
+									(')'): '.'	# one digit
+								}
+							}
+						},
+						('+-*'): {
+							('123456789'):  {	# two digits
+								('0123456789'): {	# two digits
+									(')'): '.'
+								},
+								(')'): '.'	# one digit
+							}
+						}	
+					}
+				}
+			}
+		},
+		(' '): {	# one digit
+			('('): {
+				('-'): {	# negative first expression number
+					('123456789'): {
+						('0123456789'): {	# two digits
+							('+-*'): {
+								('123456789'): {
+									('0123456789'): {	# two digits
+										(')'): '.'
+									},
+									(')'): '.'	# one digit		
+								}
+							}
+						},
+						('+-*'): {	# one digit
+							('123456789'): {
+								('0123456789'): {	# two digits
+									(')'): '.'
+								},
+								(')'): '.'	# one digit		
+							}
+						}	
+					}
+				},
+				('123456789'): {	# positive first expression number
+					('0123456789'): {	# two digits
+						('+-*'): {
+							('123456789'): {
+								('0123456789'): {	# two digits
+									(')'): '.'
+								},
+								(')'): '.'	# one digit		
+							}
+						}
+					},
+					('+-*'): {	# one digit
+						('123456789'): {
+							('0123456789'): {	# two digits
+								(')'): '.'
+							},
+							(')'): '.'	# one digit		
+						}
+					}	
+				}
+			}
+		}
+	}
+}

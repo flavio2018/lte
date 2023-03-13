81010c0f9be42fa577dbf69c8c380e8aa0986860
Flavio <flavio.petruzzellis@gmail.com>
2023-03-13 12:13:36 +0100

    Fix to_latex call

--- src/visualization/test_start2end.py
+++ src/visualization/test_start2end.py
@@ -43,7 +43,7 @@ def main(cfg):
 	plt.savefig(os.path.join(hydra.utils.get_original_cwd(),
 		f"../reports/figures/{cfg.ckpt[:-4]}_start2end{tricks}.pdf"))
 	df = df.set_index('Nesting')
-	df.T.style.to_latex(os.path.join(hydra.utils.get_original_cwd(),
+	df.T.to_latex(os.path.join(hydra.utils.get_original_cwd(),
 		f"../reports/tables/{cfg.ckpt[:-4]}_start2end{tricks}.tex"))
 
 def contain_one_space(outputs):

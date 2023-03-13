a228977478278f1b245a1c48f40d10327a141c46
Flavio <flavio.petruzzellis@gmail.com>
2023-03-13 12:25:47 +0100

    Fix number of decimals in tables

--- src/visualization/test_start2end.py
+++ src/visualization/test_start2end.py
@@ -43,6 +43,7 @@ def main(cfg):
 	plt.savefig(os.path.join(hydra.utils.get_original_cwd(),
 		f"../reports/figures/{cfg.ckpt[:-4]}_start2end{tricks}.pdf"))
 	df = df.set_index('Nesting')
+	df = np.round(df, 2)
 	df.T.to_latex(os.path.join(hydra.utils.get_original_cwd(),
 		f"../reports/tables/{cfg.ckpt[:-4]}_start2end{tricks}.tex"))
 

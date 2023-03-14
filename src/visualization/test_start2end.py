Fix num survivors computation

src/visualization/test_start2end.py
@@ -365,11 +365,12 @@ def test_ood_start2end(model, generator, max_nes, num_samples=10, tf=False, gene
 	seq_acc_values = []
 	seq_acc_std_values = []
 	nesting_values = []
-	survivors = []
+	avg_survivors = []
+	std_survivors = []
 
 	for n in range(3, max_nes+1):
 		logging.info(f"\n--- nesting {n} ---")
-		same_nes_acc, same_nes_seq_acc = np.zeros(num_samples), np.zeros(num_samples)
+		same_nes_acc, same_nes_seq_acc, same_nes_survivors = np.zeros(num_samples), np.zeros(num_samples), np.zeros(num_samples)
 
 		for sample_idx in range(num_samples):
 			values = generator.generate_batch(1, n, **generator_kwargs)
@@ -400,6 +401,7 @@ def test_ood_start2end(model, generator, max_nes, num_samples=10, tf=False, gene
 				seq_acc_avg, seq_acc_std = batch_seq_acc(output, Y[:, 1:], generator, lenY)
 				same_nes_acc[sample_idx] = acc_avg.item()
 				same_nes_seq_acc[sample_idx] = seq_acc_avg.item()
+				same_nes_survivors[sample_idx] = running.sum()
 			else:
 				same_nes_acc[sample_idx] = 0
 				same_nes_seq_acc[sample_idx] = 0
@@ -407,19 +409,22 @@ def test_ood_start2end(model, generator, max_nes, num_samples=10, tf=False, gene
 			seq_acc_values += [same_nes_seq_acc.mean()]
 			accuracy_std_values += [same_nes_acc.std()]
 			seq_acc_std_values += [same_nes_seq_acc.std()]
+			avg_survivors += [same_nes_survivors.mean()]
+			std_survivors += [same_nes_survivors.std()]
 			nesting_values += [n]
-			survivors += [running.sum()]
 
 	df = pd.DataFrame()
 	df['Character Accuracy'] = accuracy_values
 	df['Character Accuracy Std'] = accuracy_std_values
 	df['Sequence Accuracy'] = seq_acc_values
 	df['Sequence Accuracy Std'] = seq_acc_std_values
+	df['Avg Survivors'] = avg_survivors
+	df['Std Survivors'] = std_survivors
 	df['Nesting'] = nesting_values
 
 	ax = sns.barplot(data=df, x='Nesting', y='Character Accuracy', label=plot_label, ax=plot_ax, color='tab:blue')
 	if isinstance(model, ModelWrapper):
-		ax = sns.lineplot(x=range(max_nes-2), y=[s/generator_kwargs['batch_size'] for s in survivors], marker='o', color='tab:cyan')
+		ax = sns.lineplot(x=range(max_nes-2), y=[s/generator_kwargs['batch_size'] for s in avg_survivors], marker='o', color='tab:cyan')
 	return ax, df
 
 if __name__ == "__main__":